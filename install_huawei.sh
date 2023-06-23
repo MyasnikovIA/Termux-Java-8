f01ab8aeb74cfdef7e619005ba4c1b3f6539cff5
I.Myasnikov <miacom@mail.ru>
2023-06-23 12:30:16 +0700

    cr

--- install_huawei.sh
+++ install_huawei.sh
@@ -1,6 +1,6 @@
 # Архитектура ARM64
 dpkg --print-architecture  # определить архитектуру  устройства
-wget https://github.com/SHivnaTH13/Termux-OpenJDK/releases/download/v8-331/openjdk-8-jdk_8u331_all.deb --show-progress -q
+# wget https://github.com/SHivnaTH13/Termux-OpenJDK/releases/download/v8-331/openjdk-8-jdk_8u331_all.deb --show-progress -q
 dpkg -i openjdk-8-jdk_8u331_all.deb
 rm openjdk-8-jdk_8u331_all.deb
 
